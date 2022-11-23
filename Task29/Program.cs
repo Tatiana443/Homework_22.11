7833fe256f6765edefe4d1915dcbdacb61609778
Tatiana443 <tvb399@yandex.ru>
2022-11-23 10:34:14 +0600

    Project Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[8] { 1, 2, 5, 7, 19, 6, 1, 33 };
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(array);
