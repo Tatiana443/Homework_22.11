1f63ada809c9cc3c689e67b4856108021cdb5a0d
Tatiana443 <tvb399@yandex.ru>
2022-11-23 17:42:48 +0600

    Project Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = - number;
+int DigitsSum(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int digitsSum = DigitsSum(number);
+Console.WriteLine($"Сумма цифр равна {digitsSum}");
+
