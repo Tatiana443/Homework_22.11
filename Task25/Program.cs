8875a17cf076657b0dcca3dc93b2849ef32dc4d2
Tatiana443 <tvb399@yandex.ru>
2022-11-24 10:02:04 +0600

    Project Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (B < 0) B = -B;
+void DegreeNum(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine($"Number A in the degree B = {result}");
+}
+DegreeNum(A, B);
+
+
+
